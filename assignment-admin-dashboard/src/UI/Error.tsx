f5e2b2e4ff53aaa315ea65476efdae1622f08018
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-05-15 16:41:15 +0300

    feat: add some styles

--- assignment-admin-dashboard/src/UI/Error.tsx
+++ assignment-admin-dashboard/src/UI/Error.tsx
@@ -4,9 +4,14 @@ function Error() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+    <div className="mt-48 text-center">
+      <h2 className="text-xl">Something went wrong 😢</h2>
+      <button
+        className="border-1 mt-4 rounded border border-stone-700 px-4 py-2"
+        onClick={() => navigate(-1)}
+      >
+        &larr; Go back
+      </button>
     </div>
   );
 }
--- assignment-admin-dashboard/src/pages/Homepage.tsx
+++ assignment-admin-dashboard/src/pages/Homepage.tsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Homepage() {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <h3>Login to start</h3>
-      <Link to="/login">LOGIN</Link>
+    <div className="mt-10 text-center ">
+      <h1 className="text-[32px] uppercase">Welcome</h1>
+      <h3 className="mb-10 text-xl">login to start</h3>
+      <Link className="rounded bg-amber-400 px-6 py-3  text-xl" to="/login">
+        LOGIN
+      </Link>
     </div>
   );
 }
